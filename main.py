b32bc8773efd62b4a9489fc9dcbea9fd3920c84d
dhdschool <dhdschool@outlook.com>
2025-11-29 10:35:46 -0600

    Add model loading from most recent checkpoint

--- main.py
+++ main.py
@@ -4,42 +4,50 @@ from transformers import AutoModelForSequenceClassification
 import os
 import torch
 import pandas as pd
+import numpy as np 
 
 os.chdir(CURRENT_DIR)
 
 if __name__ == "__main__":
     # Train model
-    if not os.path.exists("./model/"):
+    if not os.path.exists("./model/") or os.listdir("./model/") == []:
         trainer.train()
     
+    # Find the most recent trained checkpoint
+    checkpoints = os.listdir("./model/")
+    checkpoints = [checkpoints.copy(), checkpoints.copy()]
+    checkpoints[1] = list(map(lambda x: int(x.strip("checkpoint-")), checkpoints[1]))
+    checkpoints = zip(*checkpoints)
+    checkpoints = sorted(checkpoints, key=lambda x: x[1], reverse=True)
+    most_recent_checkpoint = checkpoints[0][0]
+        
     # Load trained model
     trained_model = AutoModelForSequenceClassification.from_pretrained(
-        "./model",
+        f"./model/{most_recent_checkpoint}",
         num_labels=len(CLASSES),
         id2label=ID2CLASS,
         label2id=CLASS2ID,
         problem_type = "multi_label_classification"
     )
     
-    # Inference over non labeled dataset
-    device = torch.device("cuda" if torch.cuda.is_avaliable() else "cpu")
-    trained_model.to(device)
-    trained_model.eval()
-    
-    out_tensor = None
+    # Inference over non labeled dataset 
     n = len(TEST_DATASET["train"])
+    m = len(CLASSES) + 1
+    out_tensor = torch.empty((n, m))
+    out_tensor[:, 0] = torch.tensor(TEST_DATASET["train"]["ID"], dtype=torch.int64)
     
     for idx, entry in enumerate(TEST_DATASET["train"]):
-        inputs = {k: torch.tensor(v).unsqueeze(0).to(device) for k, v in entry.items() if k in ['input_ids', 'attention_mask']}
+        inputs = {k: torch.tensor(v).unsqueeze(0) for k, v in entry.items() if k in ['input_ids', 'attention_mask']}
         with torch.no_grad():
             outputs: torch.Tensor = trained_model(**inputs)
-            labels = (outputs.sigmoid()>.5).int()
+            labels = ((outputs.logits.sigmoid())>.5).int()
+            out_tensor[idx, 1:] = labels
+        if idx % 10 == 0:
+            print(f"Inference {idx}/{n}")
             
-            if out_tensor is None:
-                m = len(labels)
-                out_tensor = torch.empty((n, m))
-            out_tensor[idx, :] = labels
     
     # Write output to csv    
-    pd.DataFrame(out_tensor.numpy(), columns=CLASSES).to_csv("dataset/out.csv")
+    df = pd.DataFrame(out_tensor[:, 1:].numpy(), columns=CLASSES, dtype=np.int64, index=out_tensor[:, 0].numpy().astype(int))
+    df.index.name = "ID"
+    df.to_csv("dataset/out.csv")
                 
--- training.py
+++ training.py
@@ -1,14 +1,27 @@
 from transformers import TrainingArguments, Trainer, AutoModelForSequenceClassification, DataCollatorWithPadding
 from data import CLASSES, TOKENIZER, TRAIN_DATASET, CLASS2ID, ID2CLASS
 import evaluate
+import os
+import numpy as np 
 
 # Metrics to compute over the eval dataset
 metrics = evaluate.combine(["accuracy", "f1", "precision", "recall"])
 
+def sigmoid(x):
+   return 1 / (1 + np.exp(-x))
+
 # Function call provided during training
 def compute_metrics(eval_pred):
+   """Compute the metrics over a single evaluation example
+
+   Args:
+       eval_pred: A single dataset example
+
+   Returns:
+       dict: A dictionary containing the metrics specified by the evaluate library
+   """
    x, y = eval_pred
-   x = (x.sigmoid() > 0.5).astype(int).reshape(-1)
+   x = (sigmoid(x) > 0.5).astype(int).reshape(-1)
    return metrics.compute(predictions=x, references=y.astype(int).reshape(-1))
 
 dataset = TRAIN_DATASET
@@ -24,17 +37,18 @@ model = AutoModelForSequenceClassification.from_pretrained(
     problem_type = "multi_label_classification"
 )
 
+
 # Training hyperparameters
 training_args = TrainingArguments(
    output_dir="./model",
    learning_rate=2e-5,
-   per_device_train_batch_size=3,
-   per_device_eval_batch_size=3,
+   per_device_train_batch_size=2,
+   per_device_eval_batch_size=2,
    num_train_epochs=2,
    weight_decay=0.01,
    eval_strategy="epoch",
    save_strategy="epoch",
-   load_best_model_at_end=True,
+   load_best_model_at_end=True
 )
 
 trainer = Trainer(
@@ -46,3 +60,4 @@ trainer = Trainer(
    data_collator=data_collator,
    compute_metrics=compute_metrics
 )
+
